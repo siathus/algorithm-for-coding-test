4ec4c28a8705ce4e3f6220608b89ebd31abd1571
Lim Seong-Gook <sklim0932@gmail.com>
2021-05-02 06:57:27 +0900

    LeetCode > Prefix and Suffix Search

---
+++ LeetCode/src/challenge/april_2021/week2/partition_list/Solution.java
@@ -0,0 +1,39 @@
+package challenge.april_2021.week2.partition_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        int lessIndex = 0;
+
+        ListNode node = head;
+        while (node.next != null) {
+
+        }
+    }
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
