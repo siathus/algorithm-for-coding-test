505fc0d854e0faf44a16ac4689b70592badf5b7c
Lim Seong-Gook <sklim0932@gmail.com>
2021-05-04 10:00:01 +0900

    LeetCode > Partition List

--- LeetCode/src/challenge/april_2021/week2/partition_list/Solution.java
+++ LeetCode/src/challenge/april_2021/week2/partition_list/Solution.java
@@ -12,12 +12,37 @@ package challenge.april_2021.week2.partition_list;
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        int lessIndex = 0;
+        ListNode lessPartition = null;
+        ListNode lessPartitionHead = null;
+        ListNode greaterPartition = null;
+        ListNode greaterPartitionHead = null;
 
-        ListNode node = head;
-        while (node.next != null) {
-
+        while (head != null) {
+            if (head.val >= x) {
+                if (greaterPartitionHead == null) {
+                    greaterPartition = greaterPartitionHead = new ListNode(head.val);
+                } else {
+                    greaterPartition.next = new ListNode(head.val);
+                    greaterPartition = greaterPartition.next;
+                }
+            } else {
+                if (lessPartitionHead == null) {
+                    lessPartition = lessPartitionHead = new ListNode(head.val);
+                } else {
+                    lessPartition.next = new ListNode(head.val);
+                    lessPartition = lessPartition.next;
+                }
+            }
+            head = head.next;
         }
+        if (lessPartitionHead == null) {
+            return greaterPartitionHead;
+        } else if (greaterPartitionHead == null) {
+            return lessPartitionHead;
+        }
+
+        lessPartition.next = greaterPartitionHead;
+        return lessPartitionHead;
     }
 
     public class ListNode {
