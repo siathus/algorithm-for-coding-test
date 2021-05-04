7f20e57a4424a504b3f4d5f69f6549d474a35ea0
Lim Seong-Gook <sklim0932@gmail.com>
2021-05-05 08:20:01 +0900

    LeetCode > Non-decreasing Array

---
+++ LeetCode/src/challenge/may_2021/week1/non_decreasing_array/Solution.java
@@ -0,0 +1,22 @@
+package challenge.may_2021.week1.non_decreasing_array;
+
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+                
+                if (i > 0 && nums[i - 1] > nums[i + 1]) {
+                    nums[i + 1] = nums[i];
+                } else {
+                    nums[i] = nums[i + 1];
+                }
+            }
+        }
+        return true;
+    }
+}
