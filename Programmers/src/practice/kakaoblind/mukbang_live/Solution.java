ef533ad1cae9a3df40a57285fcb6fe64a784d8a2
Lim Seong-Gook <sklim0932@gmail.com>
2021-05-21 02:20:21 +0900

    프로그래머스 > 코딩테스트 연습 > 2019 KAKAO BLIND RECRUITMENT > 무지의 먹방 라이브

---
+++ Programmers/src/practice/kakaoblind/mukbang_live/Solution.java
@@ -0,0 +1,60 @@
+package practice.kakaoblind.mukbang_live;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] food_times, long k) {
+        PriorityQueue<Food> queue = new PriorityQueue<>();
+        long len = food_times.length;
+        long sum = 0;
+
+        for (int i = 0; i < len; i++) {
+            sum += food_times[i];
+            queue.offer(new Food(i + 1, food_times[i]));
+        }
+
+        if (sum <= k) return -1;
+
+        long total = 0;
+        long previous = 0;
+        while (total + ((queue.peek().getTime() - previous) * len) < k) {
+            int current = queue.poll().getTime();
+            total += (current - previous) * len;
+            len--;
+            previous = current;
+        }
+
+        List<Food> leftList = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            leftList.add(queue.poll());
+        }
+        Collections.sort(leftList, Comparator.comparingInt(Food::getIndex));
+
+        int index = leftList.get((int)((k - total) % len)).getIndex();
+
+        return index;
+    }
+
+    class Food implements Comparable<Food> {
+        private int index;
+        private int time;
+
+        public Food(int index, int time) {
+            this.index = index;
+            this.time = time;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        @Override
+        public int compareTo(Food f) {
+            return this.getTime() - f.getTime();
+        }
+    }
+}
